070cf89bdca1b4c673efb65bfc3342d75e608819
MorinehtarBlue <jimdh76@gmail.com>
2016-01-23 12:46:43 -0500

    23-Jan-16
    Added camera to tank drive and started joystick button definitions.

--- src/org/usfirst/frc/team1518/robot/Robot.java
+++ src/org/usfirst/frc/team1518/robot/Robot.java
@@ -3,8 +3,19 @@ package org.usfirst.frc.team1518.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ShapeMode;
+
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.*;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.vision.AxisCamera;
+import edu.wpi.first.wpilibj.TalonSRX;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically it 
@@ -23,12 +34,49 @@ import edu.wpi.first.wpilibj.Timer;
 public class Robot extends SampleRobot {
     RobotDrive myRobot;  // class that handles basic drive operations
     Joystick leftStick;  // set to ID 1 in DriverStation
+    Button leftTrigger = new JoystickButton(leftStick, 0);
+
     Joystick rightStick; // set to ID 2 in DriverStation
+    Button rightTrigger = new JoystickButton(rightStick, 0);
+   
+    
+    TalonSRX frontLeft, frontRight, rearLeft, rearRight;
+    int session;
+    Image frame;
+    AxisCamera camera;
+    
+    Command leftAction;
+    
+    
+    public void robotInit() {
+
+        frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+
+        // open the camera at the IP address assigned. This is the IP address that the camera
+        // can be accessed through the web interface.
+        camera = new AxisCamera("10.15.18.100");
+    }
+
+    
     public Robot() {
-        myRobot = new RobotDrive(0, 1);
+        rearLeft = new TalonSRX(0); // Left hand driving motor
+        rearRight = new TalonSRX(1); // Right hand drive motor
+        myRobot = new RobotDrive(rearLeft, rearRight); //PWM0=LEFT | PWM1=RIGHT
         myRobot.setExpiration(0.1);
+        myRobot.setMaxOutput(.75);
+
+        
+        // rearLeft.setBounds(0.75, 0.1, 0, -0.1, -0.75); // need to find updated call
+        // rearRight.setBounds(0.75, 0.1, 0, -0.1, -0.75); // need to find updated call
+        // Will likely need to make adjustment in Joystick instead of PWM's
+        
         leftStick = new Joystick(0);
         rightStick = new Joystick(1);
+        frontLeft = new TalonSRX(2);
+        //frontLeft.disable();
+        frontRight = new TalonSRX(3);
+        //frontRight.disable();
+        
     }
 
     
@@ -37,10 +85,24 @@ public class Robot extends SampleRobot {
      */
     public void operatorControl() {
         myRobot.setSafetyEnabled(true);
+        myRobot.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        myRobot.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+
+        NIVision.Rect rect = new NIVision.Rect(270, 190, 100, 100);
+
         while (isOperatorControl() && isEnabled()) {
         	myRobot.tankDrive(leftStick, rightStick);
+            camera.getImage(frame);
+            NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+
+            CameraServer.getInstance().setImage(frame);
+            
+            //look for trigger pull
+            while (leftTrigger.get()) {
+            	frontLeft.set(.25);
+            }
+            frontLeft.set(0.0);
             Timer.delay(0.005);		// wait for a motor update time
         }
     }
-
 }
---
+++ src/org/usfirst/frc/team1518/robot/leftAction.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team1518.robot;
+
+public class leftAction {
+	public leftAction() {
+		
+	}
+	protected void initialize() {
+    }
+
+    protected void execute() {
+    	
+    }
+	
+	protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+
+}
